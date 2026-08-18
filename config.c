46391340e7d0dfe7129826492ee67186aff0f458
nihiL7331 <pujanekp6@gmail.com>
2026-08-18 05:05:01 +0200

    chore: update config

--- config.c
+++ config.c
@@ -1,4 +1,8 @@
 #define SFTE_BG_OPACITY 0xEE
+#define SFTE_FONT_PATH "/usr/share/fonts/TTF/DejaVuSansMono.ttf"
+#define SFTE_FONT_BOLD_PATH "/usr/share/fonts/TTF/DejaVuSansMono-Bold.ttf"
+#define SFTE_FONT_ITALIC_PATH "/usr/share/fonts/TTF/DejaVuSansMono-Oblique.ttf"
+#define SFTE_FONT_BOLD_ITALIC_PATH "/usr/share/fonts/TTF/DejaVuSansMono-BoldOblique.ttf"
 #define SFTE_LOG_LEVEL 3
 #define SFTE_IMPL
 #include "sfte.h"
