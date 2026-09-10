c1b5add6187201736d9480fc2cd46f8979f8acf0
nihiL7331 <pujanekp6@gmail.com>
2026-09-10 02:44:02 +0200

    feat: 256-color palette support
    this fixes yazi, as before the parameters for 256-color were read as
    command identifiers and toggled the underlines

--- sfte.h
+++ sfte.h
@@ -261,12 +261,17 @@ typedef struct sfte_font_backend_info sfte_font_backend_info;
     Colors are, in order: black, red, green, yellow, blue, magenta, cyan, white.
     Indices 0 to 7 are regular colors.
     Indices 8 to 15 are bright colors.
+
+    WARN:
+    Do NOT wrap the colors in curly braces.
+    These values act as the 0-15 index colors in a 256 color palette.
 */
 #ifndef SFTE_COLOR_ANSI_PALETTE
 #define SFTE_COLOR_ANSI_PALETTE                                                                    \
-    {0x181818, 0xCC241D, 0x98971A, 0xD79921, 0x458588, 0xB16286, 0x689D6A, 0xA89984, /* regular */ \
-     0x928374, 0xFB4934, 0xB8BB26, 0xFABD2F, 0x83A598, 0xD3869B, 0x8EC07C, 0xEBDBB2} /* bright  */
-#endif  // SFTE_COLOR_ANSI_PALETTE
+    0x181818, 0xCC241D, 0x98971A, 0xD79921, 0x458588, 0xB16286, 0x689D6A, 0xA89984, /* regular */  \
+        0x928374, 0xFB4934, 0xB8BB26, 0xFABD2F, 0x83A598, 0xD3869B, 0x8EC07C,                      \
+        0xEBDBB2 /* bright  */
+#endif           // SFTE_COLOR_ANSI_PALETTE
 
 #define SFTE_COLOR_ALPHA_MASK 0xFF000000
 
@@ -1576,7 +1581,42 @@ typedef struct {
 // -------------------------------------------------------------------------------------------------
 
 static const sfte_shortcut _sfte_shortcuts[] = SFTE_SHORTCUTS;
-static const uint32_t _sfte_ansi_palette[] = SFTE_COLOR_ANSI_PALETTE;
+
+// clang-format off
+static const uint32_t _sfte_palette_256[256] = {
+    SFTE_COLOR_ANSI_PALETTE,                                                        // 0-15 standard ANSI palette
+    0x000000, 0x00005F, 0x000087, 0x0000AF, 0x0000D7, 0x0000FF, 0x005F00, 0x005F5F, // 16-231 6x6x6 RGB color cube
+    0x005F87, 0x005FAF, 0x005FD7, 0x005FFF, 0x008700, 0x00875F, 0x008787, 0x0087AF,
+    0x0087D7, 0x0087FF, 0x00AF00, 0x00AF5F, 0x00AF87, 0x00AFAF, 0x00AFD7, 0x00AFFF,
+    0x00D700, 0x00D75F, 0x00D787, 0x00D7AF, 0x00D7D7, 0x00D7FF, 0x00FF00, 0x00FF5F,
+    0x00FF87, 0x00FFAF, 0x00FFD7, 0x00FFFF, 0x5F0000, 0x5F005F, 0x5F0087, 0x5F00AF,
+    0x5F00D7, 0x5F00FF, 0x5F5F00, 0x5F5F5F, 0x5F5F87, 0x5F5FAF, 0x5F5FD7, 0x5F5FFF,
+    0x5F8700, 0x5F875F, 0x5F8787, 0x5F87AF, 0x5F87D7, 0x5F87FF, 0x5FAF00, 0x5FAF5F,
+    0x5FAF87, 0x5FAFAF, 0x5FAFD7, 0x5FAFFF, 0x5FD700, 0x5FD75F, 0x5FD787, 0x5FD7AF,
+    0x5FD7D7, 0x5FD7FF, 0x5FFF00, 0x5FFF5F, 0x5FFF87, 0x5FFFAF, 0x5FFFD7, 0x5FFFFF,
+    0x870000, 0x87005F, 0x870087, 0x8700AF, 0x8700D7, 0x8700FF, 0x875F00, 0x875F5F,
+    0x875F87, 0x875FAF, 0x875FD7, 0x875FFF, 0x878700, 0x87875F, 0x878787, 0x8787AF,
+    0x8787D7, 0x8787FF, 0x87AF00, 0x87AF5F, 0x87AF87, 0x87AFAF, 0x87AFD7, 0x87AFFF,
+    0x87D700, 0x87D75F, 0x87D787, 0x87D7AF, 0x87D7D7, 0x87D7FF, 0x87FF00, 0x87FF5F,
+    0x87FF87, 0x87FFAF, 0x87FFD7, 0x87FFFF, 0xAF0000, 0xAF005F, 0xAF0087, 0xAF00AF,
+    0xAF00D7, 0xAF00FF, 0xAF5F00, 0xAF5F5F, 0xAF5F87, 0xAF5FAF, 0xAF5FD7, 0xAF5FFF,
+    0xAF8700, 0xAF875F, 0xAF8787, 0xAF87AF, 0xAF87D7, 0xAF87FF, 0xAFAF00, 0xAFAF5F,
+    0xAFAF87, 0xAFAFAF, 0xAFAFD7, 0xAFAFFF, 0xAFD700, 0xAFD75F, 0xAFD787, 0xAFD7AF,
+    0xAFD7D7, 0xAFD7FF, 0xAFFF00, 0xAFFF5F, 0xAFFF87, 0xAFFFAF, 0xAFFFD7, 0xAFFFFF,
+    0xD70000, 0xD7005F, 0xD70087, 0xD700AF, 0xD700D7, 0xD700FF, 0xD75F00, 0xD75F5F,
+    0xD75F87, 0xD75FAF, 0xD75FD7, 0xD75FFF, 0xD78700, 0xD7875F, 0xD78787, 0xD787AF,
+    0xD787D7, 0xD787FF, 0xD7AF00, 0xD7AF5F, 0xD7AF87, 0xD7AFAF, 0xD7AFD7, 0xD7AFFF,
+    0xD7D700, 0xD7D75F, 0xD7D787, 0xD7D7AF, 0xD7D7D7, 0xD7D7FF, 0xD7FF00, 0xD7FF5F,
+    0xD7FF87, 0xD7FFAF, 0xD7FFD7, 0xD7FFFF, 0xFF0000, 0xFF005F, 0xFF0087, 0xFF00AF,
+    0xFF00D7, 0xFF00FF, 0xFF5F00, 0xFF5F5F, 0xFF5F87, 0xFF5FAF, 0xFF5FD7, 0xFF5FFF,
+    0xFF8700, 0xFF875F, 0xFF8787, 0xFF87AF, 0xFF87D7, 0xFF87FF, 0xFFAF00, 0xFFAF5F,
+    0xFFAF87, 0xFFAFAF, 0xFFAFD7, 0xFFAFFF, 0xFFD700, 0xFFD75F, 0xFFD787, 0xFFD7AF,
+    0xFFD7D7, 0xFFD7FF, 0xFFFF00, 0xFFFF5F, 0xFFFF87, 0xFFFFAF, 0xFFFFD7, 0xFFFFFF,
+    0x080808, 0x121212, 0x1C1C1C, 0x262626, 0x303030, 0x3A3A3A, 0x444444, 0x4E4E4E, // 232-255 grayscale ramp
+    0x585858, 0x626262, 0x6C6C6C, 0x767676, 0x808080, 0x8A8A8A, 0x949494, 0x9E9E9E,
+    0xA8A8A8, 0xB2B2B2, 0xBCBCBC, 0xC6C6C6, 0xD0D0D0, 0xDADADA, 0xE4E4E4, 0xEEEEEE,
+};
+// clang-format on
 static const float _sfte_font_scales[SFTE_FONT_MAX_COUNT] = SFTE_FONT_SCALES;
 
 typedef enum sfte_underline_style {
@@ -4328,17 +4368,24 @@ static inline void _sfte_csi_exec_sgr(sfte_ctx *ctx, uint16_t *p, uint16_t cnt) 
         } else if (p[i] == 27)
             ctx->term.cur_attr &= ~_SFTE_ATTR_REVERSE;
         else if (p[i] >= 30 && p[i] <= 37)  // Regular foreground
-            ctx->term.cur_fg = _sfte_ansi_palette[p[i] - 30];
+            ctx->term.cur_fg = _sfte_palette_256[p[i] - 30];
         else if (p[i] >= 90 && p[i] <= 97)  // Bright foreground
-            ctx->term.cur_fg = _sfte_ansi_palette[(p[i] - 90) + 8];
+            ctx->term.cur_fg = _sfte_palette_256[(p[i] - 90) + 8];
         else if (p[i] == 39)  // Set default foreground
             ctx->term.cur_fg = SFTE_COLOR_FG;
         else if (p[i] >= 40 && p[i] <= 47)  // Regular background
-            ctx->term.cur_bg = _sfte_ansi_palette[p[i] - 40];
+            ctx->term.cur_bg = _sfte_palette_256[p[i] - 40];
         else if (p[i] >= 100 && p[i] <= 107)  // Bright background
-            ctx->term.cur_bg = _sfte_ansi_palette[(p[i] - 100) + 8];
+            ctx->term.cur_bg = _sfte_palette_256[(p[i] - 100) + 8];
         else if (p[i] == 49)  // Set default background
             ctx->term.cur_bg = SFTE_COLOR_BG;
+        else if (p[i] == 38 && i + 2 < cnt && p[i + 1] == 5) {  // 256-color foreground
+            ctx->term.cur_fg = _sfte_palette_256[p[i + 2]];
+            i += 2;
+        } else if (p[i] == 48 && i + 2 < cnt && p[i + 1] == 5) {  // 256-color background
+            ctx->term.cur_fg = _sfte_palette_256[p[i + 2]];
+            i += 2;
+        }
         // NOTE:
         // TrueColor sequences use 5 parameters.
         // We must manually advance the `i` iterator by 4
@@ -6643,6 +6690,9 @@ static void _sfte_wayland_loop(sfte_wayland_app *app) {
         }
 #endif  // SFTE_CURSOR_BLINK
 
+        // TODO:
+        // move it to a helper for custom backends
+
         // Handle incoming Wayland events (keys, resizes)
         if (fds[0].revents & (POLLIN | POLLERR | POLLHUP))
             if (wl_display_dispatch(app->display) == -1) app->running = 0;
@@ -6890,8 +6940,7 @@ sfte_ctx *sfte_init(sfte_write_cb write_fn, void *user_data) {
 #endif  // !SFTE_NO_LOGGING
 
 #if SFTE_IMG_SIXEL
-    for (size_t i = 0; i < _SFTE_ARRAY_LEN(_sfte_ansi_palette); ++i)
-        ctx->sixel.palette[i] = SFTE_COLOR_ALPHA_MASK | _sfte_ansi_palette[i];
+    memcpy(ctx->sixel.palette, _sfte_palette_256, 256 * sizeof(uint32_t));
 #endif  // SFTE_IMG_SIXEL
 
     ctx->term.cols = SFTE_TERM_INIT_COLS;
